a74b1f41ad3bfafbe6fd23440a96b281a1b4c65c
Thierry Roger Bayala <thierrybayala@gmail.com>
2021-04-02 12:24:37 +0000

    desactivation seeds

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
-pweb:vendor/bin/heroku-php-apach2 public/
-release: php artisan migrate:fresh --seed --force && php artisan db:seed RoleSeeder
+web: vendor/bin/heroku-php-apach2 public/
+release: php artisan migrate:fresh --seed --force && php artisan
+ db:seed RoleSeeder
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         // \App\Models\Produit::factory(100)->create();
-        //  \App\Models\User::factory(10)->create();
+        //   \App\Models\User::factory(10)->create();
     }
 }
